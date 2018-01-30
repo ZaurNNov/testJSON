c2bb5246a8c2463ef46215fe98f433e83b0784ba
Zaur Giyasov <zagiyas@mera.ru>
2018-01-30 15:16:44 +0300

    Initial Commit

---
+++ testJSONTests/testJSONTests.swift
@@ -0,0 +1,36 @@
+//
+//  testJSONTests.swift
+//  testJSONTests
+//
+//  Created by Zaur Giyasov on 30/01/2018.
+//  Copyright © 2018 ZaurGiyasov. All rights reserved.
+//
+
+import XCTest
+@testable import testJSON
+
+class testJSONTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
